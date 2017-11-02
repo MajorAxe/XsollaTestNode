95bf4817296fd7b24efaf384470c591af8338d03
MajorAxe <13rgb13@gmail.com>
2017-11-02 13:33:52 +0500

    Really ready

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,9 @@ import AsyncComputed from 'vue-async-computed'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
-import VueMask from 'v-mask'
 
 Vue.use(AsyncComputed)
 Vue.use(ElementUI)
-Vue.use(VueMask)
 
 Vue.config.productionTip = false
 
